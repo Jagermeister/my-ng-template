948be7bf873f282948de9d76f508ba6a7017132f
MatthewYeager <MatthewBrandonYeager+git@gmail.com>
2019-06-09 15:28:10 -0700

    Angular 8 upgrade

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -4,11 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: '../app/home/home.module#HomeModule'
+    loadChildren: () => import('../app/home/home.module').then(m => m.HomeModule)
   },
   {
     path: 'about',
-    loadChildren: '../app/about/about.module#AboutModule'
+    loadChildren: () => import('../app/about/about.module').then(m => m.AboutModule)
   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
