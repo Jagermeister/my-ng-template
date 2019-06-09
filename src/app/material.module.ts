2299f1575575c12ef466faf5439b49783ce54a39
MatthewYeager <MatthewBrandonYeager+git@gmail.com>
2019-06-09 15:52:03 -0700

    Angular 8 + Material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {
-    MatButtonModule,
-    MatCheckboxModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatGridListModule,
-    MatSelectModule,
-    MatDialogModule,
-    MatTabsModule,
-    MatCardModule,
-    MatIconModule
-  } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
 
 const matModules = [
   MatButtonModule,
